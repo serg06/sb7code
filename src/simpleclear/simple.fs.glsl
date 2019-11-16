60dde7bf42803327deacf8e58899bd1d10b7a773
Serguei Khasimkhanov <serkhas@hotmail.com>
2019-11-15 20:03:55 -0500

    added random fragment colors (tho doesn't work cuz only one fragment atm)

--- src/simpleclear/simple.fs.glsl
+++ src/simpleclear/simple.fs.glsl
@@ -9,8 +9,21 @@
 
 out vec4 color; // output variables are sent to window/screen
 
+float rand(vec2 co);
+
 void main(void)
 {
 	//color = vs_color;
-	color = vec4(0.0, 0.0, 0.0, 1.0);
+	//color = vec4(0.0, 0.0, 0.0, 1.0);
+
+	vec2 seed = gl_FragCoord.xy;
+	vec2 seed2 = fract(seed + vec2(0.25, 0.25));
+	vec2 seed3 = fract(seed + vec2(0.50, 0.50));
+	
+	color = vec4(rand(seed), rand(seed2), rand(seed3), 1.0);
 }
+
+float rand(vec2 co){
+    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
+
